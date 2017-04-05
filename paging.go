f4fa8eea14bf473369d24b996893c7c8fb41636f
judasnow <judasnow@gmail.com>
2017-04-05 19:19:35 +0800

    分页相关

--- paging.go
+++ paging.go
@@ -1,2 +1,49 @@
 package golib
 
+
+type PageInfo struct {
+	Count int
+	Limit  int
+	Offset int
+}
+func newPageInfo(count int, limit int, offset int) PageInfo {
+	return PageInfo{
+		Count: count,
+		Limit: limit,
+		Offset: offset,
+	}
+}
+func Paging(total int, perPage int, crtPage int) PageInfo {
+	if total <= 0 {
+		return newPageInfo(0, perPage, 0)
+	}
+	if perPage <= 0 {
+		return newPageInfo(0, perPage, 0)
+	}
+	// total > 0, perPage > 0
+	var count int = ((total - 1) / perPage) + 1
+
+	if crtPage <= 0 {
+		// 默认第一页
+		return newPageInfo(count, perPage, 0)
+	}
+
+	return newPageInfo(count, perPage, (crtPage - 1) * perPage)
+}
+
+func PagingLoop(total int, perPage int, crtPage int, f func(limit int, offset int) bool) {
+	for {
+		pageInfo := Paging(total, perPage, crtPage)
+
+		if (crtPage > pageInfo.Count || crtPage <= 0) {
+			return
+		} else {
+			// loop content
+			if !f(pageInfo.Limit, pageInfo.Offset) {
+				return
+			}
+			crtPage = crtPage + 1
+		}
+
+	}
+}
---
+++ paging_test.go
@@ -0,0 +1,58 @@
+package golib
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPaging(t *testing.T) {
+	p1 := Paging(0, 0, 0)
+	if p1.Count != 0 || p1.Limit != 0 || p1.Offset != 0 {
+		t.Error("p1")
+	}
+
+	// 最后一页不够满页的情况
+	p2 := Paging(11, 2, 1)
+	if p2.Count != 6 || p2.Limit != 2 || p2.Offset != 0 {
+		t.Error("p2")
+	}
+	p22 := Paging(11, 2, 2)
+	if p22.Count != 6 || p22.Limit != 2 || p22.Offset != 2 {
+		t.Error("p22")
+	}
+	p26 := Paging(11, 2, 6)
+	if p26.Count != 6 || p26.Limit != 2 || p26.Offset != 10 {
+		t.Error("p226")
+	}
+
+	// 最后一页满页的情况
+	p3 := Paging(12, 2, 1)
+	if p3.Count != 6 || p3.Limit != 2 || p3.Offset != 0 {
+		t.Error("p3")
+	}
+}
+
+func TestPaging2(t *testing.T) {
+	// 测试分页循环
+	var tasksCount = 4
+	var perPage = 1
+	var crtPage = 1
+
+	for {
+		pageInfo := Paging(tasksCount, perPage, crtPage)
+
+		if (crtPage > pageInfo.Count || crtPage <= 0) {
+			return
+		} else {
+			// loop content
+			crtPage = crtPage + 1
+		}
+	}
+}
+
+func TestPagingLoop(t *testing.T) {
+	PagingLoop(4, 1, 1, func(limit int, offset int) bool {
+		fmt.Printf("limit: %d, offset: %d\n", limit, offset)
+		return true
+	})
+}
