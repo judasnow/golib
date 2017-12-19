c6c4de86d8ed031291541d770d3eab35cfbb47ff
judasnow <judasnow@gmail.com>
2017-12-19 10:13:06 +0800

    find

--- db/find.go
+++ db/find.go
@@ -1,29 +1,14 @@
 package db
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"fmt"
-	"errors"
+	"github.com/juju/errors"
 )
 
 var db *gorm.DB
-
-type Product struct {
-	gorm.Model
-	Code  string
-	Price uint
-}
-
-func ProductFind(key string, value interface{}, allowNull bool) (Product, error) {
-	product := Product{}
-	i, findErr := Find(key, value, allowNull, product)
-	if p, ok := i.(Product); !ok {
-		return Product{}, errors.New("返回类型错误")
-	} else {
-		return p, findErr
-	}
-}
 
 func Find(key string, value interface{}, allowNull bool, m interface{}) (interface{}, error) {
 	q := db.Where(fmt.Sprintf("%s = ?", key), value).First(&m)
@@ -36,23 +21,9 @@ func Find(key string, value interface{}, allowNull bool, m interface{}) (interfa
 	} else {
 		queryErr := q.Error
 		if queryErr != nil {
-			return m, queryErr
+			return m, errors.Annotate(queryErr, "查询失败")
 		} else {
 			return m, nil
 		}
 	}
-}
-
-func Main() {
-	var err error
-	db, err = gorm.Open("mysql", "vagrant:vagrant@(vagrant:3306)/golib?charset=utf8&parseTime=True&loc=Local")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
-	// Migrate the schema
-	db.AutoMigrate(&Product{})
-
-	p, err := ProductFind("code", "L1212", true)
-	fmt.Println(p)
 }
--- db/find_test.go
+++ db/find_test.go
@@ -1,7 +1,43 @@
 package db
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func TestMain(m *testing.M) {
-	Main()
+	"github.com/jinzhu/gorm"
+	"github.com/juju/errors"
+)
+
+func init() {
+	var err error
+	db, err = gorm.Open("mysql", "vagrant:vagrant@(vagrant:3306)/golib?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	p, err := ProductFind("code", "L1212", true)
+	fmt.Println(p)
+}
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func ProductFind(key string, value interface{}, allowNull bool) (Product, error) {
+	product := Product{}
+	i, findErr := Find(key, value, allowNull, product)
+	if p, ok := i.(Product); !ok {
+		return Product{}, errors.New("返回类型错误")
+	} else {
+		return p, findErr
+	}
+}
+
+func TestFind(t *testing.T) {
+	ProductFind("code", "L1212", false)
 }
